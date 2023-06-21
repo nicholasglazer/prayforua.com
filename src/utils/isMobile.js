fadead61294d6a2d9f07f9b52dae5c6933a65ad8
Nicholas Glazer <glazer.nicholas@gmail.com>
2023-06-21 18:17:05 +0530

    ismobile check

---
+++ src/utils/isMobile.js
@@ -0,0 +1,26 @@
+import {browser} from '$app/environment';
+
+let hasTouchScreen = false;
+
+if (browser) {
+  if ('maxTouchPoints' in navigator) {
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+  } else if ('msMaxTouchPoints' in navigator) {
+    hasTouchScreen = navigator.msMaxTouchPoints > 0;
+  } else {
+    const mQ = matchMedia?.('(pointer:coarse)');
+    if (mQ?.media === '(pointer:coarse)') {
+      hasTouchScreen = !!mQ.matches;
+    } else if ('orientation' in window) {
+      hasTouchScreen = true; // deprecated, but good fallback
+    } else {
+      // Only as a last resort, fall back to user agent sniffing
+      const UA = navigator.userAgent;
+      hasTouchScreen =
+        /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+        /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+    }
+  }
+}
+
+export default hasTouchScreen;
