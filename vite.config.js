8aa5d3157f232218b4cc06b61c515ebdcc50f016
Nicholas Glazer <glazer.nicholas@gmail.com>
2023-06-21 18:15:35 +0530

    vite aliases

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import {sveltekit} from '@sveltejs/kit/vite';
+import {defineConfig} from 'vite';
+import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  resolve: {
+    alias: {
+      $stores: path.resolve('./src/stores/'),
+      $components: path.resolve('./src/components/'),
+      $features: path.resolve('./src/features/'),
+      $utils: path.resolve('./src/utils/')
+    }
+  },
+  plugins: [sveltekit()]
 });
